4211512139424aa871b8c8854ec0f44f6f2e5223
jaykasundra2810 <88178089+jaykasundra2810@users.noreply.github.com>
2021-08-07 16:39:37 +0530

    Add files via upload

---
+++ List and tuple.py
@@ -0,0 +1,24 @@
+names = ['Jay', "Saurabh", "mihir"]
+print(names)
+
+numbers = [2, 5, 7, 9, 6]
+#numbers.sort()
+#numbers.reverse()
+
+#print(numbers[2:5])
+#print(numbers[1:5:2])
+#numbers.append(28)
+#numbers.insert(2,5)#first argument is index and other is value ou want o add.
+#numbers.append([5,6])
+#numbers.extend([5,6])
+#numbers.remove(5)
+#numbers.pop()#if index not given will pop last value
+#numbers.pop(2)
+#numbers[1] = 28 # list is mutable
+#print(numbers)
+del numbers[2]
+print(numbers)
+tp = (1,2,3,4)
+del tp
+print(tp)
+#tp[1]=5 #tuple is unmutable.
---
+++ TYpecasting.py
@@ -0,0 +1,8 @@
+a = "45"
+b = "55"
+c = "hello"
+print(a+b)
+print(int(a)+int(b))
+print(10* c )
+print(10* int(a)+int(b))
+print(10* str(int(a)+int(b)))
