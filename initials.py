f4599950c7189b5b0e68d9189fec62e1b56b097a
jaykasundra2810 <88178089+jaykasundra2810@users.noreply.github.com>
2024-11-03 13:03:12 +0530

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,8 @@
+print("J J J J J J J    K     K")
+print("      J          K   K")
+print("      J          K K ")
+print("      J          K")
+print("J     J          K K")
+print(" J    J          K   K")
+print("  J J J          K     K")
+
